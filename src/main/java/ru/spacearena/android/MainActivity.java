00068b61172661b47a3ed6efdddab28e35e8b0a7
acc15 <vyacheslav.mayorov@gmail.com>
2014-01-28 16:35:06 +0400

    Added base project

---
+++ src/main/java/ru/spacearena/android/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.spacearena.android;
+
+import android.app.Activity;
+
+/**
+ * @author Vyacheslav Mayorov
+ * @since 2014-28-01
+ */
+public class MainActivity extends Activity {
+
+
+
+}
