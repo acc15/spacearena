060f07fdb36f53d403eff6d7f3719aa71985e94f
acc15 <vyacheslav.mayorov@gmail.com>
2014-03-15 06:48:52 +0400

    Added matrix

---
+++ spacearena-core/src/test/java/ru/spacearena/engine/math/Matrix2FTest.java
@@ -0,0 +1,77 @@
+package ru.spacearena.engine.math;
+
+import org.junit.Test;
+import ru.spacearena.engine.geometry.primitives.Point2F;
+import ru.spacearena.engine.util.FloatMathUtils;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * @author Vyacheslav Mayorov
+ * @since 2014-15-03
+ */
+public class Matrix2FTest {
+
+    @Test
+    public void testTranslate() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preTranslate(-10, -10);
+        assertThat(m.transform(new Point2F(30, 40))).isEqualTo(new Point2F(20, 30));
+    }
+
+    @Test
+    public void testScale() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preScale(2f, -2f);
+        assertThat(m.transform(new Point2F(30, 40))).isEqualTo(new Point2F(60, -80));
+    }
+
+    @Test
+    public void testRotate() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preRotate(FloatMathUtils.toRadians(45f));
+        assertThat(m.transform(new Point2F(1, -1))).isEqualTo(new Point2F(FloatMathUtils.sqrt(2), 0));
+    }
+
+    @Test
+    public void testComplex() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preTranslate(-10, -10);
+        m.preRotate(FloatMathUtils.toRadians(45));
+        m.preTranslate(10, 10);
+        m.preScale(2f, -1f);
+        assertThat(m.transform(new Point2F(11, 9))).isEqualTo(new Point2F(2 * (10 + FloatMathUtils.sqrt(2)), -10f));
+    }
+
+    @Test
+    public void testIsIdentity() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        assertThat(m.isIdentity());
+    }
+
+    @Test
+    public void testInvert() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preTranslate(-10, -10);
+        m.preRotate(FloatMathUtils.toRadians(45));
+        m.preTranslate(10, 10);
+        m.preScale(2f, -1f);
+
+        final Matrix2F i = new Matrix2F(m);
+        i.invert();
+
+        i.postMultiply(m);
+        assertThat(i.isIdentity());
+    }
+
+    @Test
+    public void testInvertTransform() throws Exception {
+        final Matrix2F m = new Matrix2F();
+        m.preTranslate(-10, -10);
+        m.preRotate(FloatMathUtils.toRadians(45));
+        m.preTranslate(10, 10);
+        m.preScale(2f, -1f);
+        m.invert();
+        assertThat(m.invertTransform(new Point2F(11, 9))).isEqualTo(new Point2F(2 * (10 + FloatMathUtils.sqrt(2)), -10f));
+    }
+}
