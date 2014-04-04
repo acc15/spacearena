30bdd10ecc31434c51dcd8afe5cd6332f227a2b6
acc15 <vyacheslav.mayorov@gmail.com>
2014-04-04 07:39:17 +0400

    Removed final buffer

--- spacearena-core/src/main/java/ru/spacearena/engine/graphics/vbo/VertexBuffer.java
+++ spacearena-core/src/main/java/ru/spacearena/engine/graphics/vbo/VertexBuffer.java
@@ -11,7 +11,7 @@ import java.nio.ByteOrder;
 */
 public class VertexBuffer {
 
-    private final ByteBuffer buffer;
+    private ByteBuffer buffer;
     private VertexBufferLayout layout;
 
     public VertexBuffer(int bytes) {
